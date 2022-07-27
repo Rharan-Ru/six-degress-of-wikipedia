02559eab856675fba1f03615cf09ce335525fb26
Rharan-Ru <rharanru@gmail.com>
2022-07-27 18:19:14 -0300

    build: create requirements.txt

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/monke/Studies/WebScraping/venv"
+set -gx VIRTUAL_ENV "/home/monke/Studies/WebScraping/sixDegress/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
