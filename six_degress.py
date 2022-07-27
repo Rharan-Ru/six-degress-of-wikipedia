fe61bb5edb662f99086d6cf9e35a871c82a1a8e9
Rharan-Ru <rharanru@gmail.com>
2022-07-27 18:07:39 -0300

    refactor: Add shutdow futures and break script if link is encountered

--- six_degress.py
+++ six_degress.py
@@ -1,17 +1,16 @@
+import re
 import requests
 import collections
 from bs4 import BeautifulSoup
-import re
-from concurrent.futures import ThreadPoolExecutor
-from concurrent.futures import wait
+from concurrent.futures import ThreadPoolExecutor, wait, as_completed
 
 VISITED_LINKS = []
 DICT_NOT_VISITED_LINKS = {}
 NOT_VISITED_LINKS = []
-LAST_LINK = None
-LAST_TERM = "/WIKI/BATMAN"
-START_TERM = "/wiki/Megamind"
 
+START_TERM = "/wiki/Megamind"
+LAST_TERM = "/WIKI/BATMAN"
+SHUT_ALL = False
 
 session = requests.Session()
 
@@ -46,32 +45,44 @@ def get_all_links(link):
 
 
 def verify_links(list, parent):
+    global SHUT_ALL
     for link in list:
         if link.upper() == LAST_TERM:
             path = DICT_NOT_VISITED_LINKS[parent]["path"]
             print('\n')
-            print(f"LINK ENCONTRADO EM: {path}/{link} - DEEPTH: {len(path)}")
+            print(f'LINK ENCONTRADO NO CAMINHO: ')
+            print(f'{path} - {link} / PROFUNDIDADE DE: {len(path)}')
             print('\n')
+            SHUT_ALL = True
 
 
 def recursive_search(parent_wiki, actual_key, index):
     all_links = get_all_links(parent_wiki)
     remove_duplicates_not_visited(all_links, parent_wiki, actual_key)
-    print("----" * 20)
     verify_links(all_links, parent_wiki)
-    print(parent_wiki, index)
-    print(actual_key)
-    print(DICT_NOT_VISITED_LINKS[parent_wiki]["path"])
+    print(f'#{index} CHECKING WIKI: ', parent_wiki)
+    print('ACTUAL PARENT WIKI: ', actual_key)
+    print('PATH: ', DICT_NOT_VISITED_LINKS[parent_wiki]["path"])
+    print("----" * 20)
     if parent_wiki not in VISITED_LINKS:
         VISITED_LINKS.append(parent_wiki)
 
 
 def thread_recursive_search(list_links, actual_key):
-    global LAST_LINK
-    print(len(list_links))
-    with ThreadPoolExecutor(max_workers=22) as executor:
-        futures = [executor.submit(
-            recursive_search, list_links[i], actual_key, i) for i in range(len(list_links))]
+    global SHUT_ALL
+    print('LINKS ENCONTRADOS: ', len(list_links))
+    with ThreadPoolExecutor(max_workers=6) as executor:
+        futures = [
+            executor.submit( recursive_search, list_links[i], actual_key, i) for i in range(len(list_links))
+        ]
+        for future in as_completed(futures):
+            if future.cancelled():
+                continue
+            if SHUT_ALL:
+                executor.shutdown(wait=False)
+                print("FINALIZANDO PROCESSO")
+                [f.cancel() for f in futures if not f.done()]
+                exit()
         wait(futures)
         print("WORKING ON NEW KEY ")
         del DICT_NOT_VISITED_LINKS[actual_key]
@@ -84,7 +95,8 @@ def init_search(search_from):
     print(f"FIND CONNECTIONS BETWEEN {START_TERM.upper()} & {LAST_TERM}")
     all_links = get_all_links(search_from)
     DICT_NOT_VISITED_LINKS[search_from] = {
-        "path": [search_from], "links": all_links}
+        "path": [search_from], "links": all_links
+    }
     verify_links(all_links, search_from)
     thread_recursive_search(all_links, search_from)
 
